aa4e8c71a52b559ea43852bdb808697a79a2d07a
cerebral-caller-0985 <ashaffe@gmail.com>
2020-02-02 23:06:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,10 +1,10 @@
 # Add your code here
 def help
-  puts "I accept the following commands:"
-  puts "- help : displays the help message"
+  puts "I accept the following commands:""
+  puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts "- exit : exits the program"
+  puts "- exit : exits this program"
 end
 
 def list(song_array)
